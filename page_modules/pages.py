f418d4f0d0ff1bd7700925cd684f659e28230864
Nazar Ner <n.ner@scalr.com>
2017-03-22 14:35:20 +0200

    Fix PEP8 Errors

--- page_modules/pages.py
+++ page_modules/pages.py
@@ -9,7 +9,8 @@ farmsquery = ".test-env.scalr.com/#/farms?query="
 
 def test_goto_container(containerid):
     config.browser_name
-    browser.visit('http://' + containerid + base_url)
+    browser.visit(
+        'http://' + containerid + base_url)
 
 
 def test_login_farm(login, password):
@@ -23,25 +24,31 @@ def test_login_farm(login, password):
 
 def test_search_farm(containerid, farmid):
     config.browser_name
-    browser.visit('http://' + containerid + farmsquery + farmid)
+    browser.visit(
+        'http://' + containerid + farmsquery + farmid)
     s('.x-grid-icon-configure').click()
     time.sleep(3)
 
 
 def test_quick_start():
     config.browser_name
-    s(by.xpath("//a[.='Add farm role']")).click()
+    s(by.xpath(
+        "//a[.='Add farm role']")).click()
     time.sleep(2)
-    s(by.xpath("//*[.='Quick start']")).click()
-    s(by.xpath("//*[.='MySQL']")).click()
+    s(by.xpath(
+        "//*[.='Quick start']")).click()
+    s(by.xpath(
+        "//*[.='MySQL']")).click()
     time.sleep(3)
 
 
 def test_search_by_field(farmrole, scope):
     config.browser_name
-    s(by.xpath("//a[.='Add farm role']")).click()
+    s(by.xpath(
+        "//a[.='Add farm role']")).click()
     time.sleep(3)
-    s(by.xpath("//*[.='Base']")).click()
+    s(by.xpath(
+        "//*[.='Base']")).click()
     s("div.x-form-text-wrap-focus> input").set_value(farmrole)
     time.sleep(2)
     s(scope).click()
@@ -50,15 +57,19 @@ def test_search_by_field(farmrole, scope):
 
 def test_add_role():
     config.browser_name
-    s(by.xpath("//a[.='Add to farm']")).click()
+    s(by.xpath(
+        "//a[.='Add to farm']")).click()
     time.sleep(1)
-    s(by.xpath("//a[.='Save farm']")).click()
+    s(by.xpath(
+        "//a[.='Save farm']")).click()
     time.sleep(1)
 
 
 def test_launch(containerid, farmid):
     config.browser_name
-    browser.visit('http://' + containerid + farmsquery + farmid)
+    browser.visit(
+        'http://' + containerid + farmsquery + farmid)
     time.sleep(3)
     s(".x-grid-icon-launch").click()
     s("a.x-btn-default-small-focus").click()
+    
